48a2c0c2f1abd5b0e336f3a50cacc4779a91d92f
NTBBloodbath <bloodbathalchemist@protonmail.com>
2025-10-15 14:38:27 -0400

    feat: add hjem configs for macbook

--- hosts/macbook/default.nix
+++ hosts/macbook/default.nix
@@ -18,5 +18,30 @@
 
     # System hostname
     networking.hostName = "taiga";
+
+    # $HOME management
+    hjem.users.amartin.files = {
+      # Configuration files
+      ".config/beets".source = ../../etc/beets;
+      ".config/DankMaterialShell".source = ../../etc/DankMaterialShell-mbp;
+      ".config/fastfetch".source = ../../etc/fastfetch;
+      # ".config/fish".source = ../../etc/fish;
+      ".config/ghostty".source = ../../etc/ghostty;
+      ".config/git".source = ../../etc/git;
+      ".config/jj".source = ../../etc/jj;
+      ".config/MangoHud".source = ../../etc/MangoHud;
+      ".config/mpv".source = ../../etc/mpv;
+      ".config/niri".source = ../../etc/niri-mbp;
+      ".config/systemd".source = ../../etc/systemd;
+
+      # Bin scripts
+      ".local/bin/set_gtk_theme.sh" = {
+        source = ../../bin/set_gtk_theme.sh;
+        executable = true;
+      };
+
+      # Share files
+      ".local/share/fonts".source = ../../share/fonts;
+    };
   };
 }
