556f8e2fd17697c3aea7e1a1c69d3c15aa3f2aca
NTBBloodbath <bloodbathalchemist@protonmail.com>
2025-12-04 09:56:23 -0400

    refactor(boot): always enable zram
    chore(etc): update beets config

--- modules/boot.nix
+++ modules/boot.nix
@@ -77,7 +77,7 @@
   };
 
   # Enable ZRAM
-  zramSwap.enable = config.overworld.macbook.enable;
+  zramSwap.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
